cbf1d7deb85ce56da47e50910c98120438732da3
AnhHieuXomTrai <hieuhtph52241@gmail.com>
2025-11-05 13:09:23 +0700

    Add files via upload

---
+++ lab2.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 3; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 100);
+}
+
+const student = {
+  name: "Hieu",
+  age: 20,
+};
+student.grade = "B";
+student.age = 20;
+console.log(student);
